c7df37767b13b911d46a1cb0209c5462a85e063c
Kazi Boni Amin <kba109@sfu.ca>
2025-04-01 02:07:16 -0700

    logout fix

--- src/app/api/auth/logout/route.js
+++ src/app/api/auth/logout/route.js
@@ -1,8 +1,8 @@
 import { NextResponse } from "next/server";
 
 export async function GET(req) {
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000"; // TODO: to deploy change this
-  const res = NextResponse.redirect(new URL("/login", baseUrl));
+  const { origin } = req.nextUrl;
+  const res = NextResponse.redirect(new URL("/login", origin));
 
   // clear auth tokens
   res.cookies.set("auth_token", "", { maxAge: 0 });
